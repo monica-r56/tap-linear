c92917bc32e68a1420a64c168087e04021ee7737
cray-mx <md.rayaan.h@gmail.com>
2022-02-08 22:56:18 +0530

    fixed conflicts

--- tap_linear/tap.py
+++ tap_linear/tap.py
@@ -3,15 +3,9 @@
 from typing import List
 from singer_sdk import Tap, Stream
 from singer_sdk import typing as th
-<<<<<<< HEAD
-from tap_linear.config import api_key, api_url
-from tap_linear.streams import IssuesStream
-
-=======
 from tap_linear.streams import (
     IssuesStream
 )
->>>>>>> a1b3ebce19e334cd85884ffe813daa0a0084fa80
 STREAM_TYPES = [
     IssuesStream,
 ]
@@ -27,22 +21,13 @@ class TapLinear(Tap):
             "auth_token",
             th.StringType,
             required=True,
-<<<<<<< HEAD
-            description="The token to authenticate against the API service",
-=======
             description="The token to authenticate against the API service"
->>>>>>> a1b3ebce19e334cd85884ffe813daa0a0084fa80
         ),
         th.Property(
             "api_url",
             th.StringType,
-<<<<<<< HEAD
-            default=api_url,
-            description="The URL for the API service",
-=======
             default="https://api.linear.app/graphql",
             description="The URL for the API service"
->>>>>>> a1b3ebce19e334cd85884ffe813daa0a0084fa80
         ),
         th.Property(
             "start_date",
